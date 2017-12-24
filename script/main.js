8a8275a59e68a783d46ba9840231cfb273e2d190
AbyAbyss <abyrocksaji@gmail.com>
2017-12-24 20:25:47 +0530

    added bower type animation

--- script/main.js
+++ script/main.js
@@ -2,30 +2,31 @@
       $(document).ready(function () {
         // for parallax background
         $('.parallax').parallax();
-        // side nav bar
-        //$(".button-collapse").sideNav();
-        // Hide sideNav
-        $('.button-collapse').sideNav({
-          menuWidth: 300, // Default is 300
-          edge: 'left', // Choose the horizontal origin
-          closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-          draggable: true, // Choose whether you can drag to open on touch screens,
-        });
-
-
-        // modal with option... can also use $('.modal').modal();
-        $('.modal').modal({
-          dismissible: true, // Modal can be dismissed by clicking outside of the modal
-          opacity: .7, // Opacity of modal background
-          inDuration: 300, // Transition in duration
-          outDuration: 200, // Transition out duration
-          startingTop: '4%', // Starting top style attribute
-          endingTop: '10%', // Ending top style attribute
-        });
-        // collapse/ accordion
-        $('.collapsible').collapsible();
-        $(".title_animation").addClass("animated bounceInLeft");
-        $(".discrption").addClass("animated bounceInRight");
-        $("button").addClass("animated fadeIn");
-        $(".nav_info").addClass("animated bounceInDown");
       });
+
+
+const Portfolio = function() {
+    function typeAnimation() {
+        Typed.new("#writing-text", {
+            strings: [
+                "am a Developer.", "love everything about code.", "solve problems.","love to work with you"
+            ],
+            // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+            stringsElement: null,
+            // typing speed
+            typeSpeed: 1,
+            contentType: 'text',
+            callback: function() {
+                $("#writing-text").css({ "color": "#fff", "background-color": "#774296"});
+            },
+            preStringTyped: function() {},
+            onStringTyped: function() {}
+        });
+    }
+    return {
+        typeAnimation: typeAnimation
+    }
+
+}();
+
+Portfolio.typeAnimation();
